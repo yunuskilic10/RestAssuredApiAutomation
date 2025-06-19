7a7c9d0595d1e77edfe6141b7f7dac63bb6e235f
yunus kılıç <yunuskilic2966@gmail.com>
2025-06-19 20:05:41 +0300

    Delete yapıldı.

---
+++ src/test/java/com/otelrezervasyonu/DeleteBokingTests.java
@@ -0,0 +1,21 @@
+package com.otelrezervasyonu;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteBokingTests extends BaseTest {
+    @Test
+    public void deleteBookingTest(){
+
+
+        Response response=given().contentType(ContentType.JSON).header("Cookie","token="+createToken())
+                .when()
+                .delete("https://restful-booker.herokuapp.com/booking/"+createBookingId());
+        response.then().statusCode(201);
+        response.prettyPrint();
+
+    }
+}
