99f4eff3dd566eae8d55f277ab28b3e746fb9c27
yunus kılıç <yunuskilic2966@gmail.com>
2025-06-19 15:46:22 +0300

    RestAssuredda get isteği yapıldı.

---
+++ src/test/java/GetAllBookingsTests.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetAllBookingsTests {
+    @Test
+    public void getAllBookingTest() {
+        given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/booking")
+                .then()
+                .log().all()
+                .statusCode(200);
+
+    }
+}
